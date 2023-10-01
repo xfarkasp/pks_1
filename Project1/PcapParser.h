77d2741a5a890cdad6eb05d9f9ef3795cd0c2d2a
xfarkasp <xfarkasp@stuba.sk>
2023-10-01 15:48:15 +0200

    extension of snap and pid enum

--- Project1/PcapParser.h
+++ Project1/PcapParser.h
@@ -42,13 +42,17 @@ class PcapParser {
             IEEE_802_3_RAW = 0xFF,
         };
 
-        enum ETH_TYPE {
+        enum LLC802_2_PIDS {
+            PVSTP_PLUS = 0x010B,
             XEROX_PUP = 0x0200, //512
             PUP_ADDR_TRANS = 0x0201, //513
+            RIP = 0x0208, //513
             IPV4 = 0x0800, //2048
             X75_INTERNET = 0x0801,//2049
             X25_LVL3 = 0x0805, //2053
             ARP = 0x0806, //2054
+            CDP = 0x2000, //2054
+            DTP = 0x2004, //2054
             RARP = 0x08035, //32821
             APPLE_TALK = 0x0809B,
             APPLE_TALK_AARP = 0x080F3,
@@ -59,7 +63,9 @@ class PcapParser {
             MPLS = 0x8847,
             MPLS_UP_STREAM = 0x8848,
             PPPOE_DISCOVERY = 0x8863,
-            PPPOE_SESSION = 0x8864
+            PPPOE_SESSION = 0x8864,
+            LLDP = 0x88cc,
+            LOOPBACK = 0x9000
         };
 
         enum LLC802_2_SAP {
@@ -68,7 +74,7 @@ class PcapParser {
             LLC_SUBLAYER = 0x03,
             IP_DOD = 0x06,
             PROWAY_IEC955_NM = 0xDE,
-            BPDU = 0x42,
+            BPDU_STP = 0x42,
             MMS = 0x4E,
             ISI_IP = 0x5E,
             x_25_PLP = 0x7E,
